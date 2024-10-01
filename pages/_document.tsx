07d7768c25ffdba9fe31e0d385530a0102f22e31
Jeel <114864715+J3E1@users.noreply.github.com>
2024-10-01 11:54:28 +0530

    Update _document.tsx
    Added tapback feedback widget

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -7,6 +7,7 @@ export default function Document() {
 			<body>
 				<Main />
 				<NextScript />
+				<script src="https://tapback-widget.vercel.app/tapback-widget.umd.js" data-widget-id="e846aa7d-fd85-46dc-acb6-9b1934e65f92" defer></script>
 			</body>
 		</Html>
 	);
